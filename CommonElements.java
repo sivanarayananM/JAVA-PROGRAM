f9fb00a8ba04b9c919d32b31fb45bd8cb29ecf01
SIVANARAYANAN M <shivanarayanan9840@gmail.com>
2025-01-18 21:16:56 +0530

    Add files via upload

---
+++ CommonElements.java
@@ -0,0 +1,21 @@
+// Program to find common elements in two arrays
+public class CommonElements {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {3, 4, 5, 6, 7};
+
+        System.out.println("Common Elements: ");
+        findCommonElements(array1, array2);
+    }
+
+    public static void findCommonElements(int[] arr1, int[] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
