9962eec19f627d422fbe42d7b2a0bbb92f0760bc
SIVANARAYANAN M <shivanarayanan9840@gmail.com>
2025-01-18 19:24:00 +0530

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,24 @@
+// Program to check if a string is a palindrome
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String input = "madam";
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
