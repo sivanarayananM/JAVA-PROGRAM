a5b611c4eb6c45e5bacfeb35311c6097238ba08d
SIVANARAYANAN M <shivanarayanan9840@gmail.com>
2025-03-05 05:33:34 +0530

    Add files via upload

---
+++ MSB.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MSB {
+    public static void main(String[] args) {
+//        Scanner obj = new Scanner(System.in);
+//        System.out.println("Enter the number : ");
+//        int number = obj.nextInt();
+//        while(number>9){
+//            number=number/10;
+//        }
+//
+//        System.out.println("MSB digit of given number is : "+number);
+        char c='i';
+        int ans=((int)(c-'a'))+1;
+        System.out.println(ans);
+
+    }
+}
